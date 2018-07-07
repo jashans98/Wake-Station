31ebbe52aa3cd7ef360e6b9474e96ab933e4ab0f
Jashan Shewakramani <jashan1998s@gmail.com>
2018-07-07 14:58:37 -0700

    initial commit

---
+++ Wake Station/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  Wake Station
+//
+//  Created by Jashan Shewakramani on 2018-07-07.
+//  Copyright © 2018 Jashan Shewakramani. All rights reserved.
+//
+
+import Foundation
+
+class Colors {
+    // TODO: define app color palette here
+}
---
+++ Wake Station/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  Wake Station
+//
+//  Created by Jashan Shewakramani on 2018-07-07.
+//  Copyright © 2018 Jashan Shewakramani. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    // MARK: - Initializers
+    init() {
+        super.init(nibName: String(describing: MainViewController.self), bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
